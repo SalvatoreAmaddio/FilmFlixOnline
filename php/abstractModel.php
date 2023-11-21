6f2e9dfe9e6e53029c4cede83c1e741b2a6c18a9
SalvatoreAmaddio <141591986+SalvatoreAmaddio@users.noreply.github.com>
2023-11-21 22:25:32 +0000

    h

--- php/abstractModel.php
+++ php/abstractModel.php
@@ -1,6 +1,11 @@
 <?php
 abstract class AbstractModel 
 {
-    
+    public string $tableName;
+
+    public function select() : string 
+    {
+        return "SELECT * FROM {$this->tableName};";
+    }
 }
 ?>
--- php/database.php
+++ php/database.php
@@ -1,22 +1,62 @@
 <?php
     class Database 
     {
-        public $serverName = "localhost";
-        public $user = "admin";
-        public $pwd = "password1";
-        public $db = "FilmFlixDB";
+        public string $serverName = "localhost";
+        public string $user = "admin";
+        public string $pwd = "password1";
+        public string $db = "FilmFlixDB";
         private $conn;
-                
+        protected AbstractModel $model;
+        protected $result;
+        protected $schema;
+
         public function __construct() 
         {
-            $this->conn = mysqli_connect($this->serverName,$this->user,$this->pwd,$this->db);
-            if ($this->conn) 
-                echo 'Connected';
-            else 
-               echo 'Failed'; 
+            $this->connect();
+            $this->getColumns();
+            $this->select();
+        }
+
+        public function connect() 
+        {
+            $this->conn = mysqli_connect($this->serverName, $this->user, $this->pwd, $this->db);
+        }
+
+        public function isConnected() : bool 
+        {
+            if ($this->conn) return true;
+            return false;
+        }
+
+        public function columns() : int 
+        {
+            return $this->schema->num_rows;
+        }
+
+        public function isEmpty() : bool 
+        {
+            return $this->result->num_rows == 0;
+        }
+
+        public function getColumns() 
+        {
+            $this->schema = $this->conn->query($this->schemaQuery());
+        }
+
+        private function schemaQuery() : string
+        {
+            return "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = '{$this->db}' AND TABLE_NAME = 'tblFilms';";
+        }
+
+        public function select() 
+        {
+            $this->result = $this->conn->query("SELECT * FROM tblFilms;");
+            $arr = $this->result->fetch_assoc();
+                print_r($arr);
+
         }
     }
 
     
-new Database();    
+$db = new Database();    
 ?>
