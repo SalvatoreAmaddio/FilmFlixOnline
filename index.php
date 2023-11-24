833225d63c22c232fa8eb129e248cf82f2ca05cf
SalvatoreAmaddio <141591986+SalvatoreAmaddio@users.noreply.github.com>
2023-11-24 16:58:25 +0000

    h

--- index.php
+++ index.php
@@ -13,6 +13,14 @@
         <title>FilmFlix</title>
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
+
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Rowdies:wght@300;400;700&display=swap" rel="stylesheet">
+
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Paytone+One&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="css/file.css">
         <link rel="stylesheet" href="css/data.css">
@@ -23,14 +31,17 @@
 
     <body>
         <div id="wrapper">
-            <header>
-                <h2>Welcome to the</h2>
-                <h1>FilmFlix</h1>
-                <h1>Management System</h1>
+            <header style="background-image: url('img/projector.jpg')">
+                <img src="img/theater.png">
+                <div id="headerTitle">
+                    <h2>Welcome to the</h2>
+                    <h1><span>Film</span>Flix</h1>
+                    <h1>Management System</h1>
+                </div>
             </header>
 
             <main>
-                <section id="searchSection">
+                <section id="searchSection" style="background-image: url('img/projector.jpg')">
                     <div id="searchPanel">
                         <input id="searchBar" type="text" placeholder="Search...">
                         <button>+</button>
