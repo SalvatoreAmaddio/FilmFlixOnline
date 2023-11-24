051d8d10e6d539fce7791025f453f26c27ef2fd7
SalvatoreAmaddio <141591986+SalvatoreAmaddio@users.noreply.github.com>
2023-11-24 15:52:48 +0000

    Update FilmFormListController.php

--- php/controller/FilmFormListController.php
+++ php/controller/FilmFormListController.php
@@ -1,6 +1,7 @@
 <?php
     $x = explode("php", __DIR__);
     require_once $x[0].'/php/SAR/database.php';
+    require_once $x[0].'php/model/genre.php';
     require_once $x[0].'php/model/films.php';
 
     class FilmFormListController extends AbstractFormListController
