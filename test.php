cf6cd746c52204a728d69c80c367b285b37d8151
SalvatoreAmaddio <141591986+SalvatoreAmaddio@users.noreply.github.com>
2023-12-01 00:03:33 +0000

    j

--- test.php
+++ test.php
@@ -1,21 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="js/form.js"></script>
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
 <?php
 echo phpinfo();
-
-if (!function_exists('mysqli_init') && !extension_loaded('mysqli')) {
-    echo 'We don\'t have mysqli!!!';
-} else {
-    echo 'Phew we have it!';
-}
 ?>
