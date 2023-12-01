715b8387347ed378c73a630e9e1678017c533d82
SalvatoreAmaddio <141591986+SalvatoreAmaddio@users.noreply.github.com>
2023-12-01 02:54:39 +0000

    Update amend.php

--- amend.php
+++ amend.php
@@ -70,10 +70,12 @@
                                 </td>
                             </tr>
                         </table>
+
                         <div class="commands">
                             <button class="saveButton" value='<?php echo $controller->model()->pkfilmID?>'>💾</button>
                             <button class="deleteButton" value='<?php echo $controller->model()->pkfilmID?>'>X</button>
                         </div>
+                        
                         <div id="notification">
                             <p>Record updated!</p>
                         </div>
